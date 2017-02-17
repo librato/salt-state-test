36c1d0214beb043e7021da90bb05255de4ddcfd1
dave josephsen <dave@skeptech.org>
2017-02-17 12:30:42 -0600

    an init script to help out bootstrapping this

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+tld="test-$(openssl rand 5 -base64 | tr -d '=')"
+mkdir -p ${tld}/states/state1/test 
+
+cp test_salt_state.py $tld
+cp config.json $tld
+touch ${tld}/states/state1/init.sls
+
+for i in ${tld}/grains.json ${tld}/states/state1/test/pillar.json
+do
+	echo '{
+	    "local": {}
+		 }' > ${i}; done
+
